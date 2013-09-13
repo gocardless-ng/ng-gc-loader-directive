627ce8bbe6f0b90040329044a74ccd50d172ef1e
Philip Harrison <philip@mailharrison.com>
2013-09-13 19:08:24 +0100

    Initial

---
+++ ng-gc-loader-directive.js
@@ -0,0 +1,50 @@
+/**
+ * @license ng-gc-loader-directive v0.1.0
+ * (c) 2013-2013 GoCardless, Ltd.
+ * https://github.com/gocardless-ng/ng-gc-loader-directive.git
+ * License: MIT
+ */angular.module('loader-template.html', []).run(function($templateCache) {
+  $templateCache.put('loader-template.html',
+    '<div class="loading" ng-show="loading"><div class="loading__container"><p class="loading__text">{{ loadingStatus }}</p></div></div>');
+});
+
+'use strict';
+
+angular.module('gc.loaderController', [])
+.controller('LoaderController', [
+  '$scope',
+  function LoaderController($scope) {
+
+    $scope.$on('$routeChangeStart', function $routeChangeStart() {
+      $scope.loadingStatus = 'Loading...';
+      $scope.loading = true;
+    });
+
+    $scope.$on('$routeChangeSuccess', function $routeChangeSuccess() {
+      $scope.loading = false;
+    });
+
+    $scope.$on('$routeChangeError', function $routeChangeError() {
+      $scope.loadingStatus = 'Error while changing page';
+    });
+
+  }
+]);
+
+'use strict';
+
+angular.module('gc.loader', [
+  'gc.loaderController',
+  'loader-template.html'
+]).directive('loader',
+  [function loaderDirective() {
+
+    return {
+      restrict: 'E',
+      templateUrl: 'loader-template.html',
+      replace: true,
+      controller: 'LoaderController',
+      scope: {}
+    };
+
+  }]);
---
+++ readme.md
@@ -0,0 +1,7 @@
+# ng-gc-loader-directive
+
+![](https://circleci.com/gh/gocardless-ng/ng-gc-loader-directive.png?circle-token=:circle-token)
+
+## Copyright and license
+
+Copyright 2013 GoCardless, Inc under the MIT license.
